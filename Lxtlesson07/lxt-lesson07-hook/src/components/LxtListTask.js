8e7580c116096044c7623e958558f47e7f1ceff3
toan-dz <ltoan5861@gmail.com>
2024-06-11 15:17:07 +0700

    Lxtlesson07

---
+++ Lxtlesson07/lxt-lesson07-hook/src/components/LxtListTask.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+export default function LxtListTask(renderLxtListTask, onLxtTaskEdit,onTaskDelete) {
+  console.log(renderLxtListTask);
+  //ham xu ly
+  const lxtHandleEdit = (param) => {
+    console.log("Click edit:", param);
+    onLxtTaskEdit(param); //
+  };
+  //xoa 
+  const lxtHandleDelete = (param)=>{
+    if (window.confim('bạn có chắc chắn xóa không')){
+        onTaskDelete(param)
+    }
+  }
+  
+
+  //render data
+  let lxtElmentTask = renderLxtListTask.map((task, is, index) => {
+    return (
+      <>
+        <tr key={index}>
+          <td>{index + 1} </td>
+          <td>{index.lxt_taskId} </td>
+          <td>{index.lxt_taskName} </td>
+          <td>{index.lxt_level} </td>
+          <td>
+            <button
+              className="btn btn-success"
+              onClick={() => lxtHandleEdit(task)}
+            >
+              Edit
+            </button>
+            <button
+              className="btn btn-danger"
+              onClick={() => lxtHandleDelete(task)}
+            
+            > Remove
+            </button>
+          </td>
+        </tr>
+      </>
+    );
+  });
+  return (
+    <div>
+      <h2>danh sách các nhiệm vụ</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>STT</th>
+            <th>Task ID</th>
+            <th>Task Name</th>
+            <th>Task Level</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>{lxtElmentTask}</tbody>
+      </table>
+    </div>
+  );
+}
---
+++ Lxtlesson07/lxt-lesson07-hook/src/components/LxtTaskAdd.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+
+export default function LxtTaskAdd({ renderLxtTask, LxtOnSubmit }) {
+  // const LxtTaskObj = {
+  //   lxt_taskId: 0,
+  //   lxt_taskName: "",
+  //   lxt_level: "",
+  // };
+  const [LxTask, setLxtTask] = useState(renderLxtTask);
+  useEffect(() => {
+    setLxtTask(renderLxtTask);
+  }, [renderLxtTask]);
+  //
+const lxtTieuDe = renderLxtIsAddOrEdit==true?"Thêm mới task":"Sửa thông tin task"
+  //
+  const lxtHanderchange = (lxtEvent) => {
+    let name = lxtEvent.target.name;
+    let value = lxtEvent.target.value;
+    setLxtTask((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+  const lxtHanderSubmit = (lxtEvent) => {
+    lxtEvent.preventDefault();
+    LxtOnSubmit(LxTask);
+  };
+  return (
+    <div>
+      <h2>{lxtTieuDe}</h2>
+      <from>
+        <div className="input-group- mb-3">
+          <span className="input-group- mb-3" id="basic-addon1">
+            Task ID
+          </span>
+
+          <input
+            type="text"
+            name="lxt_taskId"
+            value={LxTask.lxt_taskId}
+            onChange={"lxtHanderchange"}
+            class="form-control"
+            placeholder="Usename"
+            aria-label="Usename"
+            aria-describedby="basic-addon1"
+          />
+        </div>
+        <div>
+          <span className="input-group- mb-3" id="basic-addon1">
+            Task Name
+          </span>
+
+          <input
+            name="lxt_taskName"
+            value={LxTask.lxt_taskName}
+            onChange={"lxtHanderchange"}
+            class="form-control"
+            placeholder="Usename"
+            aria-label="Usename"
+            aria-describedby="basic-addon1"
+          />
+        </div>
+        <div>
+          <label>Task Level</label>
+          <select
+            name="lxt_level"
+            value={LxTask.lxt_level}
+            onChange={"lxtHanderchange"}
+          >
+            <option value={"Small"}>Small</option>
+            <option value={"Medium"}>Medium</option>
+            <option value={"High"}>High</option>
+          </select>
+        </div>
+        <button onClick={lxtHanderSubmit}>Ghi Lại</button>
+      </from>
+    </div>
+  );
+}
